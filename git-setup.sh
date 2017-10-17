0599e783e550e2efd26f44f25d36d6d64ea8a63f
Raouf Aghrout <raouf@thelabrador.co.uk>
2017-10-17 19:21:28 +0100

    added git config script from gists

---
+++ git-setup.sh
@@ -0,0 +1,10 @@
+git config --global user.name "Raouf Aghrout"
+git config --global user.email raouf@something.com
+git config --global push.default simple
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.lg "log --all --branches --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
